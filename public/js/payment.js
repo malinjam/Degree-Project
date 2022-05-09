e398f18c86a7086bfd88e1a2a9ce259758b2bc37
malinasdfghjkl <malin.marques@gmail.com>
2022-05-09 17:49:30 +0200

    notifications enabled

---
+++ public/js/payment.js
@@ -0,0 +1,22 @@
+var showBookingNotification = function() {
+    navigator.serviceWorker.ready.then(function(registration) {
+      registration.showNotification("Reservation Received", {
+        body:
+          "Thank you for booking with Flyone\n"+
+          "You will be notified if your chnages to your flight occur. ",
+        icon: "/icons-images/apple-touch-icon.png",
+        badge: "/favicon.ico",
+        tag: "Booking Complete"
+      });
+    });
+  };
+  
+  var checkNotification = function() {
+    if ("Notification" in window && "serviceWorker" in navigator) {
+      Notification.requestPermission().then(function(permission){
+        if (permission === "granted") {
+          showBookingNotification();
+        }
+      });
+    }
+  };
